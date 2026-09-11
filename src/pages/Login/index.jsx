e804db40d2d37e4e43d72ade7450e42e73b4b0c6
Alexandra Chanteloup <275184320+AlexCh79@users.noreply.github.com>
2026-09-11 16:15:18 +0200

    Rédaction page Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,25 @@
+import './login.css'
+
 function Login() {
     return (
-        <div>
-            <h4>Login</h4>
+        <div className="login">
+            <div className="left-column">
+                <div className="form-container">
+                    <form className="login-form" method="post" action="/dashboard">
+                        <h3>Transformez vos stats en résultats</h3>
+                        <h4>Se connecter</h4>
+                        <label htmlFor="username">Nom d'utilisateur :</label>
+                        <input id="username" name="username" alt="pseudo" />
+                        <label htmlFor="password">Mot de passe :</label>
+                        <input type="password" id="password" name="password" />
+                        <button className="blue-btn" type="submit">Se connecter</button>
+                        <span>Mot de passe oublié ?</span>
+                    </form>
+                </div>
+            </div>
+            <div className="background-container">
+                <img src="/background_picture.png" alt="" />
+            </div>
         </div>
     )
 }
